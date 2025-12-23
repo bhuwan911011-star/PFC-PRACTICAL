a5e54d2a0a90a6311f4754687843a2a2629ee2e7
bhuwan911011-star <bhuwan911011@gmail.com>
2025-12-23 23:27:47 +0530

    Add files via upload

---
+++ pratical8.c
@@ -0,0 +1,65 @@
+// #include <stdio.h>
+// int sumOfDigits(int n) {
+//     int sum = 0;
+//     while (n > 0) {
+//         sum += n % 10;  
+//         n /= 10;        
+//     }
+//     return sum;
+// }
+
+// int main() {
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+
+//     printf("Sum of digits = %d\n", sumOfDigits(num));
+
+//     return 0;
+// }
+
+ 
+//#include <stdio.h>
+//void swap(int *x, int *y) {
+  //  int temp;
+    // temp = *x;   
+    //*x = *y;    
+    //*y = temp;
+//}
+
+// int main() {
+//     int a, b;
+
+//     printf("Enter two numbers: ");
+//     scanf("%d %d", &a, &b);
+
+//     printf("Before swap: a = %d, b = %d\n", a, b);
+
+//     swap(&a, &b);
+
+//     printf("After swap: a = %d, b = %d\n", a, b);
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// void printTriangle(int n) {
+//     int i, j;
+//     for (i = 1; i <= n; i++) {
+//         for (j = 1; j <= i; j++) {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main() {
+//     int n;
+//     printf("Enter height of triangle: ");
+//     scanf("%d", &n);
+
+//     printTriangle(n);
+
+//     return 0;
+// }
